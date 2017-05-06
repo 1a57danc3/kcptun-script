c5162022b66eed9eca95fee38c7827f0497f67a0
ky0ncheng <kyoncheng@yahoo.co.jp>
2017-05-07 04:07:32 +0800

    update

--- client-start.sh
+++ client-start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /Users/ky0n/Dropbox/Git/kcptun-bash-shortcut
-./client_darwin_amd64 -r "remote_ip:554" -l "127.0.0.1:2333" -mtu 1400 -sndwnd 256 -rcvwnd 2048 -mode fast2 -dscp 46 > kcptun.log 2>&1 &
+./client_darwin_amd64 -r "remote_ip:29900" -l "127.0.0.1:8388" -mtu 1350 -mode fast2 > kcptun.log 2>&1 &
 echo "Kcptun started."
--- server-start.sh
+++ server-start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /root/kcptun/
-./server_linux_amd64 -t "server_ip:8388" -l "local_ip:554" -mtu 1400 -sndwnd 2048 -rcvwnd 2048 -mode fast2 > kcptun.log 2>&1 &
+./server_linux_amd64 -t "server_ip:29900" -l "local_ip:2333" -mtu 1350 -mode fast2 > kcptun.log 2>&1 &
 echo "Kcptun started."
